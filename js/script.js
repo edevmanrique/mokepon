81b0ae2950b84c50bf52748bd697e97afc3f3ce5
edmanriquer_ <emanrique80713@ufide.ac.cr>
2023-07-08 17:58:26 -0600

    Adding pets dynamically to HTML

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+//It starts the game after DOM is generated correctly
 function startGame(){
     let petButton = document.getElementById('select-pet__select');
     petButton.addEventListener('click', selectPet);
 }
 
+//We get the selected pet by the player
 function selectPet(){
     let player = '';
 
@@ -14,11 +16,14 @@ function selectPet(){
     let electricDarknessPet = document.getElementById('thunderneg')
 
     if(firePet.checked){
-        alert('You have selected to Burntrack')
+        alert('You have selected to Burntrack 🔥')
+        setName('Burntrack 🔥')
     } else if(electricPet.checked){
-        alert('You have selected to Glitch')
+        alert('You have selected to Glitch ⚡')
+        setName('Glitch ⚡')
     } else if(darknessPet.checked){
-        alert('You have selected to Mortus')
+        alert('You have selected to Mortus 🖤')
+        setName('Mortus 🖤')
     } else if(fireElectricPet.checked){
         alert('You have selected to Inferstorm')
     } else if(fireDarknessPet.checked){
@@ -28,6 +33,12 @@ function selectPet(){
     } else {
         alert('You have to select a pet');
     }
+}
+
+//We get the name of selectPet function to add it dynamically to the battle section
+function setName(name){
+    let petName = document.getElementById('select-attack__my-pet');
+    petName.innerHTML = name;
 }
 
 window.addEventListener('load', startGame);
