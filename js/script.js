724038c5f0c083a6c88b7a0cbb4e0c15f2fb6a83
edmanriquer_ <emanrique80713@ufide.ac.cr>
2023-07-08 17:45:18 -0600

    Selecting pets in JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
-function selectPet(){
-    alert('Pet selected')
+function startGame(){
+    let petButton = document.getElementById('select-pet__select');
+    petButton.addEventListener('click', selectPet);
 }
 
-let petButton = document.getElementById('select-pet__select');
-petButton.addEventListener('click', selectPet);
+function selectPet(){
+    let player = '';
+
+    let firePet = document.getElementById('burntrack')
+    let electricPet = document.getElementById('glitch')
+    let darknessPet = document.getElementById('mortus')
+    let fireElectricPet = document.getElementById('inferstorm')
+    let fireDarknessPet = document.getElementById('blazen')
+    let electricDarknessPet = document.getElementById('thunderneg')
+
+    if(firePet.checked){
+        alert('You have selected to Burntrack')
+    } else if(electricPet.checked){
+        alert('You have selected to Glitch')
+    } else if(darknessPet.checked){
+        alert('You have selected to Mortus')
+    } else if(fireElectricPet.checked){
+        alert('You have selected to Inferstorm')
+    } else if(fireDarknessPet.checked){
+        alert('You have selected to Blazen')
+    } else if(electricDarknessPet.checked){
+        alert('You have selected to Thunderneg')
+    } else {
+        alert('You have to select a pet');
+    }
+}
+
+window.addEventListener('load', startGame);
